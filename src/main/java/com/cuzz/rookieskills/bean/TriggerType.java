ccd4449949ae555840dbb51307703aff8f1a4eeb
RookieCuzz <85008507+1875417678@users.noreply.github.com>
2024-07-29 17:19:51 +0800

    111

--- src/main/java/com/cuzz/rookieskills/bean/TriggerType.java
+++ src/main/java/com/cuzz/rookieskills/bean/TriggerType.java
@@ -1,5 +1,5 @@
 package com.cuzz.rookieskills.bean;
 
 public enum TriggerType {
-    ShootBow,Timer,Attack,RightClick,Damaged,Damaged_By_Entity,Left_Click,;
+    ShootBow,Timer,Attack,RightClick,Damaged,Damaged_By_Entity,Left_Click,Key;
 }
